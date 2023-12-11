a33f2597037dc7dd6b34a86d0ee17ff5bda91334
Jummelmarijuan <142652721+Jummelmarijuan@users.noreply.github.com>
2023-12-11 21:01:39 +0900

    final

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,48 +1,84 @@
-// Buy Now
+const modalOpenButtons = document.querySelectorAll('[data-open-modal]');
+const modalCloseButtons = document.querySelectorAll('[data-close-modal]');
+const body = document.querySelector('body');
+let isModalOpen = false;
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-1]"),
-    closeModalBtn: document.querySelector("[data-modal-close-1]"),
-    modal: document.querySelector("[data-modal-1]"),
-  };
+modalCloseButtons.forEach(closeBtn => {
+  closeBtn.addEventListener('click', e => {
+    modalClose(closeBtn.closest('[data-modal]'));
+  });
+});
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+modalOpenButtons.forEach(modalBtn => {
+  modalBtn.addEventListener('click', e => {
+    const modalName = modalBtn.getAttribute('data-open-modal');
+    const currentModal = document.querySelector(`[data-modal="${modalName}"]`);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    modalOpen(currentModal);
+  });
+});
+
+function modalOpen(currentModal) {
+  const form = currentModal.querySelector('form');
+
+  currentModal.classList.remove('is-hidden');
+  bodyLock();
+  isModalOpen = true;
+
+  document.addEventListener('keydown', onEscOrClick);
+  currentModal.addEventListener('click', onEscOrClick);
+
+  function onEscOrClick(e) {
+    if (e.which === 27 || e.target === currentModal) {
+      modalClose(currentModal);
+      document.removeEventListener('keydown', onEscOrClick);
+      currentModal.removeEventListener('click', onEscOrClick);
+    }
   }
-})();
 
-// Leave a review
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-2]"),
-    closeModalBtn: document.querySelector("[data-modal-close-2]"),
-    modal: document.querySelector("[data-modal-2]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  if (!form) {
+    return;
   }
-})();
 
-// Subscribe
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-3]"),
-    closeModalBtn: document.querySelector("[data-modal-close-3]"),
-    modal: document.querySelector("[data-modal-3]"),
-  };
+  form.addEventListener('submit', onSubmit);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  function onSubmit(e) {
+    e.preventDefault();
+    modalClose(currentModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    const modalName = form.getAttribute('data-form');
+    if (modalName) {
+      const currentModal = document.querySelector(`[data-modal="${modalName}"]`);
+      modalOpen(currentModal);
+    }
+
+    e.target.reset();
+    form.removeEventListener('submit', onSubmit);
   }
-})();
+}
+
+function modalClose(modalActive) {
+  modalActive.classList.add('is-hidden');
+  isModalOpen = false;
+  modalActive.addEventListener('transitionend', onTransitionEnd);
+
+  function onTransitionEnd() {
+    modalActive.removeEventListener('transitionend', onTransitionEnd);
+    bodyUnLock();
+  }
+}
+
+function bodyLock() {
+  const scrollbarWidth = window.innerWidth - body.offsetWidth + 'px';
+  const bodyPaddingRight = body.style.paddingRight;
+
+  body.style.overflow = 'hidden';
+  body.style.paddingRight = !bodyPaddingRight ? scrollbarWidth : bodyPaddingRight;
+}
+
+function bodyUnLock() {
+  if (!isModalOpen) {
+    body.style.removeProperty('overflow');
+    body.style.removeProperty('padding-right');
+  }
+}
